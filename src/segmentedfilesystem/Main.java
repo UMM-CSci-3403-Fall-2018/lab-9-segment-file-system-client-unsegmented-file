1a777acbf04312e37cc5f61c063c72d1d46b5275
bella148 <bella148@morris.umn.edu>
2018-11-27 11:41:23 -0600

    Initial commit, begin work on lab

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -1,9 +1,78 @@
 package segmentedfilesystem;
 
-public class Main {
-    
-    public static void main(String[] args) {
-        
+import java.io.IOException;
+import java.net.*;
+import java.util.ArrayList;
+
+public class Main
+{
+    private static DatagramSocket socket;
+    private static InetAddress server;
+    private static int port = 6014;
+
+    private byte[] small;
+    private byte[] AsYouLikeIt;
+    private byte[] binary;
+
+    public static ArrayList<Byte> bigStorage = new ArrayList<Byte>();
+
+    public static PacketController smallController = new PacketController();
+    public static PacketController AsYouLikeItController = new PacketController();
+    public static PacketController binaryController = new PacketController();
+
+    public static void main(String[] args)
+    {
+        //
+        try
+        {
+            server = InetAddress.getByName("heartofgold.morris.umn.edu");
+        }
+        catch(UnknownHostException e)
+        {
+            System.out.println(e);
+        }
+
+        //
+        try
+        {
+            socket = new DatagramSocket();
+        }
+        catch(SocketException e)
+        {
+            System.out.println(e);
+        }
+
+
+        byte[] buff = new byte[0];
+        DatagramPacket helloPacket = new DatagramPacket(buff, buff.length, server, port);
+
+        try
+        {
+            socket.send(helloPacket);
+        }
+        catch(IOException e)
+        {
+            System.out.println(e);
+        }
+
+
+        byte[] bufferPacket = new byte[1024 + 4];
+        DatagramPacket bigPacket = new DatagramPacket(bufferPacket, bufferPacket.length);
+
+        //Need to keep requesting packets until we have all of them
+        while(!smallController.isFinished && !AsYouLikeItController.isFinished && !binaryController.isFinished)
+        {
+            try
+            {
+                socket.receive(bigPacket);
+            }
+            catch(IOException e)
+            {
+                System.out.println(e);
+            }
+        }
+
+
     }
 
 }
---
+++ src/segmentedfilesystem/PacketController.java
@@ -0,0 +1,49 @@
+package segmentedfilesystem;
+
+public class PacketController
+{
+    public int size = 0;
+    public int maxSize = 0;
+    public boolean hasHeader = false;
+    public boolean isFinished = false;
+
+    public int getSize()
+    {
+        return size;
+    }
+
+    public void setSize(int newMaxSize)
+    {
+        this.size += newMaxSize;
+
+        if(this.size > 0 && this.size == this.maxSize)
+        {
+            this.isFinished = true;
+        }
+    }
+
+    public int getMaxSize()
+    {
+        return maxSize;
+    }
+
+    public void setMaxSize(int newMaxSize)
+    {
+        this.maxSize = newMaxSize;
+    }
+
+    public boolean getHeader()
+    {
+        return hasHeader;
+    }
+
+    public void setHasHeader(boolean newHasHeader)
+    {
+        this.hasHeader = newHasHeader;
+    }
+
+    public boolean getIsFinished()
+    {
+        return this.isFinished;
+    }
+}
