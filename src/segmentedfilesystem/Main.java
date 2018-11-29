6c3152c67aba20a12c65156cd0a8d59664c2cf9f
bella148 <bella148@morris.umn.edu>
2018-11-29 11:26:10 -0600

    Moved repeat logic into individual functions

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -17,10 +17,10 @@ public class Main
     public static PacketController File2 = new PacketController();
     public static PacketController File3 = new PacketController();
 
+    public static HashMap<Integer, Integer> fileMap = new HashMap<Integer, Integer>();
+
     public static void main(String[] args) throws UnknownHostException, SocketException, IOException
     {
-
-        HashMap<Integer, Integer> fileMap = new HashMap<Integer, Integer>();
 
         server = InetAddress.getByName("heartofgold.morris.umn.edu");
 
@@ -41,25 +41,25 @@ public class Main
 
             try
             {
+                //Request a packet
                 socket.receive(bigPacket);
 
-                //System.out.println("Server not broken");
-                System.out.println(bufferPacket[0]);
-                System.out.println(bufferPacket[1]);
+                //If the file ID is not in the HashMap, add it
+                HashLogic(bufferPacket[1]);
+
                 //Determine what type of packet we get from the server
                 switch(bufferPacket[0])
                 {
                     //Packet is even, so it is a header packet
                     case 0:
 
-                        //If the file ID is not in the HashMap, add it
-                        if(!fileMap.containsKey(bufferPacket[1]))
-                        {
-                            //Add a new entry to the next spot in the HashMap
-                            fileMap.put((int)bufferPacket[1], fileMap.size() + 1);
-                        }
-
-                        String name = "";
+                        // Header packet
+                        // bufferPacket[0] = status bye
+                        // bufferPacket[1] = file ID
+                        // bufferPacket[2] = filename
+                        // bufferPacket[3] = filename
+                        // bufferPacket[4] = filename
+                        // etc..
 
                         //Constructing the filenames for each respective file using a HashMap to look up which file ID corresponds to which file
                         switch(fileMap.get((int)bufferPacket[1]))
@@ -67,43 +67,17 @@ public class Main
                             //Belongs to File1
                             case 1:
 
-                                for(int i = 0; i < bigPacket.getLength() - 2; i++)
-                                {
-                                    byte charByte = bufferPacket[i + 2];
-                                    name += (char)charByte;
-                                }
-
-                                System.out.println(name);
-
-                                File1.setFileName(name);
-
+                                File1.setFileName(ReadHeaderFilename(bufferPacket, bigPacket.getLength()));
                                 break;
 
                             case 2:
 
-                                for(int i = 0; i < bigPacket.getLength() - 2; i++)
-                                {
-                                    byte charByte = bufferPacket[i + 2];
-                                    name += (char)charByte;
-                                }
-
-                                System.out.println(name);
-
-                                File2.setFileName(name);
-
+                                File2.setFileName(ReadHeaderFilename(bufferPacket, bigPacket.getLength()));
                                 break;
 
                             case 3:
 
-                                for(int i = 0; i < bigPacket.getLength() - 2; i++)
-                                {
-                                    byte charByte = bufferPacket[i + 2];
-                                    name += (char)charByte;
-                                }
-
-                                System.out.println(name);
-
-                                File3.setFileName(name);
+                                File3.setFileName(ReadHeaderFilename(bufferPacket, bigPacket.getLength()));
                                 break;
                         }
 
@@ -112,11 +86,50 @@ public class Main
                     //Packet is odd, so it is a data packet
                     case 1:
 
+                        // Data packet
+                        // bufferPacket[0] = status bye
+                        // bufferPacket[1] = file ID
+                        // bufferPacket[2] = packet number part 1
+                        // bufferPacket[3] = packet number part 2
+                        // bufferPacket[4] = data
+                        // bufferPacket[5] = data
+                        // bufferPacket[6] = data
+                        // etc..
+
+                        System.out.println(bufferPacket[2] + " " + bufferPacket[3]);
+
+                        switch(fileMap.get((int)bufferPacket[1]))
+                        {
+                            //Belongs to File1
+                            case 1:
+                                for(int i = 0; i < bigPacket.getLength(); i++)
+                                {
+
+                                }
+                                break;
+
+                            case 2:
+                                break;
+
+                            case 3:
+                                break;
+                        }
 
                         break;
 
                     //Packet is the last data packet for a given file
                     case 3:
+
+                        // Final data packet
+                        // bufferPacket[0] = status bye
+                        // bufferPacket[1] = file ID
+                        // bufferPacket[2] = packet number part 1
+                        // bufferPacket[3] = packet number part 2
+                        // bufferPacket[4] = data
+                        // bufferPacket[5] = data
+                        // bufferPacket[6] = data
+                        // etc..
+
                         break;
                 }
 
@@ -126,6 +139,33 @@ public class Main
                 System.out.println("More" + e);
             }
         }
+
+    }
+
+    public static void HashLogic(byte bufferPacket)
+    {
+        //If the file ID is not in the HashMap, add it
+        if(!fileMap.containsKey(bufferPacket))
+        {
+            //Add a new entry to the next spot in the HashMap
+            fileMap.put((int)bufferPacket, fileMap.size() + 1);
+        }
+    }
+
+    public static String ReadHeaderFilename(byte[] bufferPacket, int length)
+    {
+        String name = "";
+        for(int i = 0; i < length - 2; i++)
+        {
+            byte charByte = bufferPacket[i + 2];
+            name += (char)charByte;
+        }
+
+        return name;
+    }
+
+    public static void ReadDataPacket(byte[] bufferPacket)
+    {
 
     }
 
