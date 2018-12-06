cd6780b232059cc90cd66c9048700cabaa588a74
bella148 <bella148@morris.umn.edu>
2018-12-06 10:47:55 -0600

    Finish lab by writing methods to output the files

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -11,31 +11,32 @@ public class Main
     private static InetAddress server;
     private static int port = 6014;
 
+    //Create objects to keep track of each file
     public static PacketController File1 = new PacketController();
     public static PacketController File2 = new PacketController();
     public static PacketController File3 = new PacketController();
 
+    //Create has map to map a generic file to a File1, File2, or File3
     public static HashMap<Integer, Integer> fileMap = new HashMap<Integer, Integer>();
 
     public static void main(String[] args) throws UnknownHostException, SocketException, IOException
     {
 
+        //Begin to 'connect' to the server by sending a UDP packet
         server = InetAddress.getByName("heartofgold.morris.umn.edu");
-
         socket = new DatagramSocket();
 
-        byte[][] storage = new byte[0][0];
-
+        //Actually send the hello message to initiate packet sending
         byte[] buff = new byte[0];
         DatagramPacket helloPacket = new DatagramPacket(buff, buff.length, server, port);
-
         socket.send(helloPacket);
 
+        //Create a packet that can contain the max amount of bytes in a packet
         byte[] bufferPacket = new byte[1024 + 4];
         DatagramPacket bigPacket = new DatagramPacket(bufferPacket, bufferPacket.length);
 
         //Need to keep requesting packets until we have all of them
-
+        System.out.println("Start getting packets from server...");
         while(!File1.getIsFinished() || !File2.getIsFinished() || !File3.getIsFinished())
         {
             try
@@ -66,16 +67,19 @@ public class Main
                             //Belongs to File1
                             case 1:
 
+                                //Set the filename and mark the file as having received the header packet;
                                 File1.setFileName(ReadHeaderFilename(bufferPacket, bigPacket.getLength()));
                                 File1.setHasHeader(true);
                                 break;
 
+                            //Duplicate logic to case 1, but for File2
                             case 2:
 
                                 File2.setFileName(ReadHeaderFilename(bufferPacket, bigPacket.getLength()));
                                 File2.setHasHeader(true);
                                 break;
 
+                            //Duplicate logic to case 1, but for File3
                             case 3:
 
                                 File3.setFileName(ReadHeaderFilename(bufferPacket, bigPacket.getLength()));
@@ -98,19 +102,24 @@ public class Main
                         // bufferPacket[6] = data
                         // etc..
 
+                        //Create a packet number from the two bytes
                         int PacketNumber = MakePacketNumber(bufferPacket[2], bufferPacket[3]);
 
+                        //We need to determine which file this data packet belongs to. So we look into the hash mash.
                         switch(fileMap.get((int)bufferPacket[1]))
                         {
                             //Belongs to File1
                             case 1:
+                                //Add that data packet into that file's storage at the correct position
                                 File1.AddElement(PacketNumber, ReadDataPacket(bufferPacket, bigPacket.getLength()));
                                 break;
 
+                            //Duplicate logic but for File2
                             case 2:
                                 File2.AddElement(PacketNumber, ReadDataPacket(bufferPacket, bigPacket.getLength()));
                                 break;
 
+                            //Duplicate logic but for File3
                             case 3:
                                 File3.AddElement(PacketNumber, ReadDataPacket(bufferPacket, bigPacket.getLength()));
                                 break;
@@ -131,15 +140,21 @@ public class Main
                         // bufferPacket[6] = data
                         // etc..
 
+                        //Construct packet number
                         int LastPacketNumber = MakePacketNumber(bufferPacket[2], bufferPacket[3]);
 
+                        //This is the last data packet for a file, and we need to determine which file
                         switch(fileMap.get((int)bufferPacket[1]))
                         {
 
                             //Belongs to File1
                             case 1:
                                 System.out.println("Got File1 last packet");
+
+                                //Set the max size to be equal to the packet number we found plus one (we treated the first packet as packet 1, not 0, oops)
                                 File1.setMaxSize(LastPacketNumber + 1);
+
+                                //Add that data packet into that file's storage at the correct position
                                 File1.AddElement(LastPacketNumber, ReadDataPacket(bufferPacket, bigPacket.getLength()));
                                 break;
 
@@ -160,6 +175,7 @@ public class Main
                         break;
                 }
 
+                //Debugging, having packet loss has occurred, so if the program hangs, try enabling this to see what happened.
                 //System.out.println(File1.getIsFinished() + " " + File2.getIsFinished() + " " + File3.getIsFinished());
 
             }
@@ -169,8 +185,11 @@ public class Main
             }
         }
 
+        //Tell the controller to build the files
         System.out.println("Read to process files");
-        //Make a File.BuildContents method
+        File1.BuildFile();
+        File2.BuildFile();
+        File3.BuildFile();
 
     }
 
@@ -186,9 +205,14 @@ public class Main
 
     public static String ReadHeaderFilename(byte[] bufferPacket, int length)
     {
+        //Get the name from the header packet.
         String name = "";
+
+        //We do minus 2 here since we constructed a generic packet of size 1024 + 4 to handle both a header, and data packet. Since the header
+        //has two less bytes, we just need to ignore them.
         for(int i = 0; i < length - 2; i++)
         {
+            //Offset the position so we can read the data
             byte charByte = bufferPacket[i + 2];
             name += (char)charByte;
         }
@@ -199,9 +223,15 @@ public class Main
 
     public static byte[] ReadDataPacket(byte[] bufferPacket, int length)
     {
+        //Get the data from the data packet
+
+        //We do minus 4 here since the first four bytes are all info, and the data is after
         byte[] data = new byte[length - 4];
+
+        //Read all of the data in the packet, offsetting by 4
         for(int i = 0; i < length - 4; i++)
         {
+            //Offset by 4 to get the data
             byte dataByte = bufferPacket[i + 4];
             data[i] = dataByte;
         }
@@ -211,9 +241,11 @@ public class Main
 
     public static int MakePacketNumber(byte LOB, byte HOB) {
 
+        //Cast the byte as an int
         int iLOB = (int)LOB;
         int iHOB = (int)HOB;
 
+        //If the bytes are negative, add 256 to them since java doesn't support unsigned ints
         if (iLOB < 0)
         {
             iLOB = iLOB + 256;
@@ -224,6 +256,7 @@ public class Main
             iHOB = iHOB + 256;
         }
 
+        //Do math to convert to proper packet number
         int result = 256 * iLOB + iHOB;
 
         //System.out.println(result + " packet");
--- src/segmentedfilesystem/PacketController.java
+++ src/segmentedfilesystem/PacketController.java
@@ -1,5 +1,8 @@
 package segmentedfilesystem;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class PacketController
@@ -8,8 +11,8 @@ public class PacketController
     public int maxSize = 0;
     public boolean hasHeader = false;
     public boolean isFinished = false;
-    public String filename ="";
-    public ArrayList<byte[]> storage = new ArrayList<>();
+    public String filename = "";
+    public ArrayList<byte[]> storage = new ArrayList<byte[]>();
 
     public void setMaxSize(int newMaxSize)
     {
@@ -41,21 +44,27 @@ public class PacketController
         this.filename = newfilename;
     }
 
+    //Adds a packet number into the correct spot in storage so we don't have to sort later
     public void AddElement(int position, byte[] data)
     {
+        //Make sure the arraylist is large enough
         ensureSize(storage, position + 1);
 
+        //Set it's position
         storage.set(position, data);
 
         size++;
         System.out.println("File " + getFileName() + " " + size + " out of " + maxSize);
 
+        //If we have everything we need, then we can mark the file as complete
         if(maxSize > 0 && size == maxSize && getHeader())
         {
             isFinished = true;
         }
     }
 
+    // This is a taxing process. To better this, instead of increasing size each time, it would be better to
+    // start at some value (maybe 50) and double it each time we need it.
     public void ensureSize(ArrayList<byte[]> list, int size)
     {
         // Prevent excessive copying while we're adding
@@ -63,6 +72,45 @@ public class PacketController
         while (list.size() < size)
         {
             list.add(null);
+        }
+    }
+
+    //Builds the file from all of the bytes collected
+    public void BuildFile() throws IOException
+    {
+
+        System.out.println("Writing file " + filename);
+
+        //Gets the total amount of bytes
+        int numBytes = 0;
+        for(int i = 0; i < storage.size(); i++)
+        {
+            numBytes += storage.get(i).length;
+        }
+
+        //Creates the 'file' in terms of bytes
+        byte[] finishedFile = new byte[numBytes];
+
+        int tempSize = 0;
+
+        //Adds the bytes to the file
+        for(int i = 0; i < storage.size(); i++)
+        {
+            for(int o = 0; o < storage.get(i).length; o++)
+            {
+                finishedFile[tempSize] = storage.get(i)[o];
+                tempSize++;
+            }
+        }
+
+        //Writes the file to directory
+        try(FileOutputStream fos = new FileOutputStream("output/" + filename))
+        {
+            fos.write(finishedFile);
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println(e);
         }
     }
 }
